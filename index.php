681465e14c329eb896a20024ff010b801b2651c9
Walter Junior <juniordeonisio@gmail.com>
2017-05-16 13:09:43 -0300

    Up project JediPHP

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once("config.php");
+
+/*
+$sql = new Sql();
+
+$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+
+echo json_encode($usuarios);
+*/
+
+$root = new Usuario();
+
+$root->loadById(2);
+
+echo $root;
+
+
+?>
