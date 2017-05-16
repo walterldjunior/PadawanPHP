6a38af8aa55e7f197b2ed16294677dc571c81cb3
Walter Junior <juniordeonisio@gmail.com>
2017-05-16 14:12:59 -0300

    creater func login in Usuario

--- index.php
+++ index.php
@@ -4,17 +4,30 @@ require_once("config.php");
 
 /*
 $sql = new Sql();
-
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
 echo json_encode($usuarios);
-*/
+
 
 $root = new Usuario();
-
 $root->loadById(2);
-
 echo $root;
+
+
+
+// chamando um metodo static - CLASSE::METODO
+$lista = Usuario::getList();
+echo json_encode($lista);
+
+
+$search = new Usuario();
+$search = Usuario::search("jo");
+echo json_encode($search);
+*/
+
+$usuario = new Usuario();
+$usuario->login("marco","qwerty");
+echo $usuario;
+
 
 
 ?>
